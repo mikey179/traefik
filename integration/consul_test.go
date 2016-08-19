846618dcdf0e095d7585c1bbbc22e4107771fb8d
Frank Kleine <frank.kleine@1und1.de>
2016-08-19 17:28:02 +0200

    add integration test using Consul and traefik/alias

--- integration/consul_test.go
+++ integration/consul_test.go
@@ -11,12 +11,13 @@ import (
 	"github.com/go-check/check"
 
 	"errors"
-	"github.com/containous/traefik/integration/utils"
-	"github.com/containous/traefik/provider"
-	checker "github.com/vdemeester/shakers"
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/containous/traefik/integration/utils"
+	"github.com/containous/traefik/provider"
+	checker "github.com/vdemeester/shakers"
 )
 
 // Consul test suites (using libcompose)
@@ -425,5 +426,138 @@ func (s *ConsulSuite) TestCommandStoreConfig(c *check.C) {
 
 		c.Assert(string(p.Value), checker.Equals, value)
 
+	}
+}
+func (s *ConsulSuite) TestAliasConfiguration(c *check.C) {
+	s.setupConsul(c)
+	consulHost := s.composeProject.Container(c, "consul").NetworkSettings.IPAddress
+	file := s.adaptFile(c, "fixtures/consul/simple.toml", struct{ ConsulHost string }{consulHost})
+	defer os.Remove(file)
+	cmd := exec.Command(traefikBinary, "--configFile="+file)
+	err := cmd.Start()
+	c.Assert(err, checker.IsNil)
+	defer cmd.Process.Kill()
+
+	whoami1 := s.composeProject.Container(c, "whoami1")
+	whoami2 := s.composeProject.Container(c, "whoami2")
+	whoami3 := s.composeProject.Container(c, "whoami3")
+	whoami4 := s.composeProject.Container(c, "whoami4")
+
+	backend1 := map[string]string{
+		"traefik_configurations/1/backends/backend1/circuitbreaker/expression": "NetworkErrorRatio() > 0.5",
+		"traefik_configurations/1/backends/backend1/servers/server1/url":       "http://" + whoami1.NetworkSettings.IPAddress + ":80",
+		"traefik_configurations/1/backends/backend1/servers/server1/weight":    "10",
+		"traefik_configurations/1/backends/backend1/servers/server2/url":       "http://" + whoami2.NetworkSettings.IPAddress + ":80",
+		"traefik_configurations/1/backends/backend1/servers/server2/weight":    "1",
+	}
+	backend2 := map[string]string{
+		"traefik_configurations/2/backends/backend1/circuitbreaker/expression": "NetworkErrorRatio() > 0.5",
+		"traefik_configurations/2/backends/backend1/servers/server1/url":       "http://" + whoami3.NetworkSettings.IPAddress + ":80",
+		"traefik_configurations/2/backends/backend1/servers/server1/weight":    "1",
+		"traefik_configurations/2/backends/backend1/servers/server2/url":       "http://" + whoami4.NetworkSettings.IPAddress + ":80",
+		"traefik_configurations/2/backends/backend1/servers/server2/weight":    "2",
+	}
+	frontend1 := map[string]string{
+		"traefik_configurations/1/frontends/frontend1/backend":            "backend1",
+		"traefik_configurations/1/frontends/frontend1/entrypoints":        "http",
+		"traefik_configurations/1/frontends/frontend1/priority":           "1",
+		"traefik_configurations/1/frontends/frontend1/routes/test_1/rule": "Host:test.localhost",
+	}
+	frontend2 := map[string]string{
+		"traefik_configurations/2/frontends/frontend1/backend":            "backend1",
+		"traefik_configurations/2/frontends/frontend1/entrypoints":        "http",
+		"traefik_configurations/2/frontends/frontend1/priority":           "1",
+		"traefik_configurations/2/frontends/frontend1/routes/test_2/rule": "Host:test.localhost",
+	}
+	for key, value := range backend1 {
+		err := s.kv.Put(key, []byte(value), nil)
+		c.Assert(err, checker.IsNil)
+	}
+	for key, value := range backend2 {
+		err := s.kv.Put(key, []byte(value), nil)
+		c.Assert(err, checker.IsNil)
+	}
+	for key, value := range frontend1 {
+		err := s.kv.Put(key, []byte(value), nil)
+		c.Assert(err, checker.IsNil)
+	}
+	for key, value := range frontend2 {
+		err := s.kv.Put(key, []byte(value), nil)
+		c.Assert(err, checker.IsNil)
+	}
+
+	aliasErr := s.kv.Put("traefik/alias", []byte("traefik_configurations/1"), nil)
+	c.Assert(aliasErr, checker.IsNil)
+
+	// wait for consul
+	err = utils.Try(60*time.Second, func() error {
+		_, err := s.kv.Exists("traefik_configurations/2/frontends/frontend1/routes/test_1/rule")
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	c.Assert(err, checker.IsNil)
+
+	// wait for traefik
+	err = utils.TryRequest("http://127.0.0.1:8081/api/providers", 60*time.Second, func(res *http.Response) error {
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		if !strings.Contains(string(body), "test_1") {
+			return errors.New("Incorrect traefik config")
+		}
+		return nil
+	})
+	c.Assert(err, checker.IsNil)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://127.0.0.1:8000/", nil)
+	c.Assert(err, checker.IsNil)
+	req.Host = "test.localhost"
+	response, err := client.Do(req)
+
+	c.Assert(err, checker.IsNil)
+	c.Assert(response.StatusCode, checker.Equals, 200)
+
+	body, err := ioutil.ReadAll(response.Body)
+	c.Assert(err, checker.IsNil)
+	if !strings.Contains(string(body), whoami1.NetworkSettings.IPAddress) &&
+		!strings.Contains(string(body), whoami2.NetworkSettings.IPAddress) {
+		c.Log("Invalid ip addresses on initial request")
+		c.Fail()
+	}
+
+	switchErr := s.kv.Put("traefik/alias", []byte("traefik_configurations/2"), nil)
+	c.Assert(switchErr, checker.IsNil)
+
+	// wait again for traefik
+	err = utils.TryRequest("http://127.0.0.1:8081/api/providers", 60*time.Second, func(res *http.Response) error {
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		if !strings.Contains(string(body), "test_2") {
+			return errors.New("Incorrect traefik config after switch")
+		}
+		return nil
+	})
+	c.Assert(err, checker.IsNil)
+
+	req2, err := http.NewRequest("GET", "http://127.0.0.1:8000/", nil)
+	c.Assert(err, checker.IsNil)
+	req2.Host = "test.localhost"
+	response2, err := client.Do(req2)
+
+	c.Assert(err, checker.IsNil)
+	c.Assert(response2.StatusCode, checker.Equals, 200)
+
+	body, err = ioutil.ReadAll(response2.Body)
+	c.Assert(err, checker.IsNil)
+	if !strings.Contains(string(body), whoami3.NetworkSettings.IPAddress) &&
+		!strings.Contains(string(body), whoami4.NetworkSettings.IPAddress) {
+		c.Log("Invalid ip addresses after switch")
+		c.Fail()
 	}
 }
